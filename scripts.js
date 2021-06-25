160d173bd3e5befb5e8690cc9736dc90a145578a
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2021-06-25 07:32:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Kulwinder Kaur");
+$("#policyNumber").text("D037673294 / 24062021");
+$("#startDate").text("24-Jun-2021");
+$("#endDate").text("23-Jun-2022");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08DC6986");
+$("#make").text("HONDA");
+$("#model").text("ACTIVA");
+$("#vehicalSubType").text("3G");
+$("#engineNo").text("JF50E82250095");
+$("#chassis").text("ME4JF504JF8250510");
